46b9f5e663aa23073da16f9a3100acf874fa9dfd
Vo THanh NHan <sieunhan1397@gmail.com>
2019-06-17 15:02:29 +0700

    calculator

--- Calculator/js/script.js
+++ Calculator/js/script.js
@@ -20,32 +20,32 @@ $(document).ready(function(){
       $('#screen').val(res);
       //$('#gido').val(eval($('#gido').text()));//
 
-  }
+  };
   num = function(n){
     value = $('#screen').val();
     value += n;
     $('#screen').val(value);
-  }
-  clr = function(){
+  };
+  clearScreen = function(){
     $('#screen').val("");
-  }
+  };
 
 // Restricts input for each element in the set of matched elements to the given inputFilter.
-(function($) {
-  $.fn.inputFilter = function(inputFilter) {
-    return this.on("input keydown keyup mousedown mouseup select contextmenu drop", function() {
-      if (inputFilter(this.value)) {
-        this.oldValue = this.value;
-        this.oldSelectionStart = this.selectionStart;
-        this.oldSelectionEnd = this.selectionEnd;
-      } else if (this.hasOwnProperty("oldValue")) {
-        this.value = this.oldValue;
-        this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
-      }
-    });
-  };
-}(jQuery));
-$("#screen").inputFilter(function(value) {
-  return /^\d*$/.test(value);
-});
+  (function($) {
+    $.fn.inputFilter = function(inputFilter) {
+      return this.on("input keydown keyup mousedown mouseup select contextmenu drop", function() {
+        if (inputFilter(this.value)) {
+          this.oldValue = this.value;
+          this.oldSelectionStart = this.selectionStart;
+          this.oldSelectionEnd = this.selectionEnd;
+        } else if (this.hasOwnProperty("oldValue")) {
+          this.value = this.oldValue;
+          this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
+        }
+      });
+    };
+  }(jQuery));
+  $("#screen").inputFilter(function(value) {
+    return /^\d*$/.test(value);
+  });
 });
