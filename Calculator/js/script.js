3af8b1aba75c08f4a2f18a1475c4392cf8abbae1
Vo THanh NHan <sieunhan1397@gmail.com>
2019-06-17 13:22:36 +0700

    chinh sua thu muc

---
+++ Calculator/js/script.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+
+    $('.calc-btn').on('click', function(e) {
+        let btn = e.target.innerHTML;
+        num(btn);
+    });
+    $('.calc-btn-special').on('click', function(e) {
+        let btn = e.target.innerHTML;
+        num(btn);
+    });
+
+  $(document).bind('keypress', function(e) {
+      if(e.keyCode==13){
+      $('#cal').trigger('click');
+      }
+  });
+  showResult = function() {
+      console.log(eval($('#screen').val()));
+      var res = eval($('#screen').val());
+      $('#screen').val(res);
+      //$('#gido').val(eval($('#gido').text()));//
+
+  }
+  num = function(n){
+    value = $('#screen').val();
+    value += n;
+    $('#screen').val(value);
+  }
+  clr = function(){
+    $('#screen').val("");
+  }
+});
